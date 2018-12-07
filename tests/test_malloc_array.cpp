c110f6c21b4d8118100262a359ff0bdd9c823141
Joshua Weinstein <jweinstein@splunk.com>
2018-12-06 18:38:04 -0800

    more array tests

--- tests/test_malloc_array.cpp
+++ tests/test_malloc_array.cpp
@@ -17,10 +17,33 @@ void test_array_push()
 	TEST_CHECK_ALL(arr.getLen() == 2, failureCount);
 }
 
+void test_array_access()
+{
+	MallocArray<int> arr;
+	arr.push(1);
+	arr.push(2);
+	arr.push(3);
+	TEST_CHECK_ALL(arr[0] == 1 && arr[1] == 2, failureCount);
+}
+
+void test_array_expand()
+{
+	int i;
+	MallocArray<int> arr(10);
+	for(i = 0; i < 12; i++)
+	{
+		arr.push(i);
+	}
+	TEST_CHECK_ALL(arr.getCap() > 10, failureCount);
+	TEST_CHECK_ALL(arr.getLen() == 12, failureCount);
+}
+
 int main(int argc, char const *argv[])
 {
 	test_array_init();
 	test_array_push();
+	test_array_access();
+	test_array_expand();
 	if(failureCount > 0) {
 		std::cout << "TESTS FAILED\n";
 		return 3;
